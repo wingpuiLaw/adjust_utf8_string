9a0a6fb077ea374fef2e7b2ddba7e257b32fb94b
pure <wipelaw@users.noreply.github.com>
2017-09-09 19:12:09 +0800

    Create adjuest_utf8_string.c

---
+++ adjuest_utf8_string.c
@@ -0,0 +1,37 @@
+U8* adjust_end_of_utf8(U8* source, U8 lenth){
+	U8 *p = source;
+	U8 i = 0;
+	for (U8 i = 0; i < lenth; ){
+		if (*p == 0)  return source;
+		if (*p >= 0xF0){
+			if (i + 4 < lenth){
+				p += 4;  i += 4;
+				continue;
+			}
+			else{  *p = 0;  return source;  }
+		}
+		else if (*p >= 0xE0){
+			if (i + 3 < lenth){
+				p += 3;  i += 3;
+				continue;
+			}
+			else{  *p = 0;  return source;  }
+		}
+		else if (*p >= 0xC0){
+			if (i + 2 < lenth){
+				p += 2;  i += 2;
+				continue;
+			}
+			else{  *p = 0;  return source;  }
+		}
+		else if (*p < 0x80){
+			if (i + 1 < lenth){
+				++p;  ++i;
+				continue;
+			}
+			else{  *p = 0;  return source;  }
+		}
+		else  *p = 0;
+	}
+	return source;
+}
